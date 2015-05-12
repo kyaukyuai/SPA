695d601348de8b62a178b59e1c756d6259f55977
Yuya Kakui <mail@kakuiyuya.com>
2015-05-12 23:06:36 +0900

    p.140

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -8,21 +8,35 @@ newcap: true, nomen: true, plusplus: true,
 regexp: true, sloppy: true, vars: false,
 white: true
 */
-/*global $, spa */
+/*global $, spa, getComputedStyle */
 
 spa.chat = (function () {
   var
     configMap = {
       main_html: String()
-      + '<div style="padding:1em; color#fff;">'
-        + 'Say hello to chat'
-      + '</div>',
-    settable_map: {}
-  },
-  stateMap = { $container: null },
-  jqueryMap = {},
+        + '<div class="spa-chat">'
+          + '<div class="spa-chat-head>'
+            + '<div class="spa-chat-head-toggle">+</div>'
+            + '<div class="spa-chat-head-title">'
+              + 'Chat'
+            + '</div>'
+          + '</div>'
+          + '<div class="spa-chat-closer">x</div>'
+          + '<div class="spa-chat-sizer">'
+            + '<div class="spa-chat-msgs"></div>'
+            + '<div class="spa-chat-box">'
+              + '<input type="text"/>'
+              + '<div>send</div>'
+            + '</div>'
+          + '</div>'
+        + '</div>',
 
-  setJqueryMap, configModule, initModule;
+      settable_map: {}
+    },
+    stateMap = { $container: null },
+    jqueryMap = {},
+
+    setJqueryMap, configModule, initModule;
   // -----------------------------------------
   setJqueryMap = function () {
     var $container = stateMap.$container;
