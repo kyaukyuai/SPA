9a78f8e7c7983c8cf3900372771f7743af2bc5dd
Yuya Kakui <mail@kakuiyuya.com>
2015-05-13 08:41:39 +0900

    p.142

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -31,16 +31,64 @@ spa.chat = (function () {
           + '</div>'
         + '</div>',
 
-      settable_map: {}
+      settable_map: {
+        slider_open_time: true,
+        slider_close_time: true,
+        slider_opened_em: true,
+        slider_closed_em: true,
+        slider_opened_title: true,
+        slider_closed_title: true,
+
+        chat_model: true,
+        people_model: true,
+        set_chat_anchor: true
+      },
+
+      slider_open_time: 250,
+      slider_close_time: 250,
+      slider_opened_em: 16,
+      slider_closed_em: 2,
+      slider_opened_title: 'Click to close',
+      slider_closed_title: 'Click to open',
+
+      chat_model: null,
+      people_model: null,
+      set_chat_anchor: null
     },
-    stateMap = { $container: null },
+    stateMap = {
+      $append_target: null,
+      position_type: 'closed',
+      px_per_em: 0,
+      slider_hidden_px: 0,
+      slider_closed_px: 0,
+      slidr_opened_px: 0
+    },
     jqueryMap = {},
 
-    setJqueryMap, configModule, initModule;
+    setJqueryMap, getEmSize, setPxSizes, setSliderPosition,
+    onClickToggle, configModule, initModule;
   // -----------------------------------------
+  getEmSize = function ( elem ) {
+    return Number(
+      getComputedStyle( elem, '' ).fontSize.match(/¥d*¥.?¥d*/)[0]
+    );
+  };
+
   setJqueryMap = function () {
-    var $container = stateMap.$container;
-    jqueryMap = { $container: $container };
+    var
+      $append_target = stateMap.$append_target,
+      $slider = $append_target.find( '.spa-chat' );
+
+    jqueryMap = {
+      $slider:  $slider,
+      $head: $slider.find( '.spa-chat-head' ),
+      $toggle: $slider.find( '.spa-chat-head-toggle' ),
+      $title: $slider.find( '.spa-chat-head-title' ),
+      $sizer: $slider.find( '.spa-chat-sizer' ),
+      $msgs: $slider.find( '.spa-chat-msgs' ),
+      $box: $slider.find( '.spa-chat-box' ),
+      $input: $slider.find( '.spa-chat-input input[type=text]' )
+    };
   };
 
   configModule = function ( input_map ) {
